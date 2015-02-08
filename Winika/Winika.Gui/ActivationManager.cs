e8a2e73cb64c7b6a756c97bcf769b66218d7f23f
nXu <nXu@nXu.hu>
2015-02-08 20:54:15 +0100

    Updated GUI, added lib code

---
+++ Winika/Winika.Gui/ActivationManager.cs
@@ -0,0 +1,31 @@
+﻿namespace Winika.Gui
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.Win32;
+
+    public class ActivationManager
+    {
+        public string GetMachineGuid()
+        {
+            const string location = @"SOFTWARE\Microsoft\Cryptography";
+            const string name = "MachineGuid";
+
+            using (var localMachineX64View =
+                RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                using (var rk = localMachineX64View.OpenSubKey(location))
+                {
+                    if (rk == null)
+                        throw new KeyNotFoundException("0-DOESNT-WORK");
+
+                    var machineGuid = rk.GetValue(name);
+                    if (machineGuid == null)
+                        throw new IndexOutOfRangeException("1-DOESNT-WORK");
+
+                    return machineGuid.ToString().ToUpper();
+                }
+            }
+        }
+    }
+}
---
+++ Winika/Winika.Lib/VersionTools/VersionManager.cs
@@ -0,0 +1,30 @@
+﻿namespace Winika.Lib.VersionTools
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Tool for reading the version of an assembly-
+    /// </summary>
+    public static class VersionReader
+    {
+        /// <summary>
+        /// Gets the version of the current assembly.
+        /// </summary>
+        /// <returns>Version of the current assembly.</returns>
+        public static Version GetVersion()
+        {
+            return GetVersion(Assembly.GetExecutingAssembly());
+        }
+
+        /// <summary>
+        /// Gets the version of a given assembly.
+        /// </summary>
+        /// <param name="assembly">Assembly to get the version of.</param>
+        /// <returns>Version of the provided assembly.</returns>
+        public static Version GetVersion(Assembly assembly)
+        {
+            return assembly.GetName().Version;
+        }
+    }
+}
